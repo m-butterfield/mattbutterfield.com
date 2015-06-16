c7ede0f92f5134879448bfe68341a18d37bab6cb
Matt Butterfield <matt.butterfield@percolate.com>
2015-06-15 20:39:01 -0400

    clean up pagination in api

--- app/post/resources.py
+++ app/post/resources.py
@@ -2,27 +2,30 @@
 Resources for Posts
 
 """
-from flask.ext.restful import abort, fields, marshal_with, Resource
+from flask.ext.restful import abort, fields, marshal, Resource
 
 from app import db
 from app.post.models import Post
 
+POST_PAGINATION_FIELDS = {
+    'next_post_id': fields.String(attribute='next_post.id'),
+    'previous_post_id': fields.String(attribute='previous_post.id'),
+}
 
 POST_FIELDS = {
     'id': fields.String,
     'text': fields.String,
     'created_at': fields.DateTime,
     'image_url': fields.String,
-    'next_post_id': fields.String(attribute='next_post.id'),
-    'previous_post_id': fields.String(attribute='previous_post.id'),
 }
 
 
 class PostResource(Resource):
 
-    @marshal_with(POST_FIELDS)
     def get(self, post_id):
         post = db.session.query(Post).get(post_id)
         if not post:
             abort(404, message="Post {} not found".format(post_id))
-        return post
+        data = marshal(post, POST_PAGINATION_FIELDS, envelope='pagination')
+        data.update(marshal(post, POST_FIELDS, envelope='data'))
+        return data
