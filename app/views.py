9c234b9d64781e225e60f02c3ec2c5d946c34293
Matt Butterfield <matt.butterfield@percolate.com>
2015-06-18 23:49:54 -0400

    add 404 error handler

--- app/views.py
+++ app/views.py
@@ -20,3 +20,8 @@ def index(post_id=None):
     else:
         post = post_api.get_most_recent_post()
     return render_template('index.html', post=json.dumps(serialize_post(post)))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Not found :(', 404
