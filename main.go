d42d8f3fe7427bcb79347596cfc71a89a3d25456
Matt Butterfield <matt.butterfield@percolate.com>
2016-12-15 21:17:55 -0500

    servce basic template

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
+type Page struct {
+	Caption  string
+	ImageURL string
+}
+
+var indexTemplate = template.New("index.html")
+
+func serve(res http.ResponseWriter, req *http.Request) {
+	page := Page{
+		Caption:  "Hello dude",
+		ImageURL: "http://images.mattbutterfield.com/20070909_010.jpg",
+	}
+	err := indexTemplate.Execute(res, page)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello.")
+	indexTemplate.ParseFiles("index.html")
+	http.HandleFunc("/", serve)
+	fmt.Println("Serving...")
+	http.ListenAndServe(":8000", nil)
 }
