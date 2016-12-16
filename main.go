939c0cfb59a2558bb14054c8db12a4ec558c8ee1
Matt Butterfield <matt.butterfield@percolate.com>
2016-12-15 22:10:09 -0500

    proof of concept

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Page struct {
@@ -11,22 +14,40 @@ type Page struct {
 	ImageURL string
 }
 
-var indexTemplate = template.New("index.html")
+var (
+	db *sql.DB
+	indexTemplate *template.Template
+)
 
 func serve(res http.ResponseWriter, req *http.Request) {
-	page := Page{
-		Caption:  "Hello dude",
-		ImageURL: "http://images.mattbutterfield.com/20070909_010.jpg",
-	}
-	err := indexTemplate.Execute(res, page)
+	err := indexTemplate.Execute(res, getRandomPage())
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "app.db")
+	if err != nil {
+		panic(err)
+	}
+	indexTemplate = template.New("index.html")
 	indexTemplate.ParseFiles("index.html")
 	http.HandleFunc("/", serve)
 	fmt.Println("Serving...")
 	http.ListenAndServe(":8000", nil)
+}
+
+func getRandomPage() Page {
+	var imageID, caption string
+	row := db.QueryRow("SELECT id, caption FROM images ORDER BY RANDOM() LIMIT 1")
+	err := row.Scan(&imageID, &caption)
+	if err != nil {
+		panic(err)
+	}
+	return Page{
+		Caption: caption,
+		ImageURL: "http://images.mattbutterfield.com/" + imageID,
+	}
 }
