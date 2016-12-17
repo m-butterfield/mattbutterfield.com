9c007919a2cba9e9975cf50dab250024af437c5e
Matt Butterfield <matt.butterfield@percolate.com>
2016-12-17 14:34:51 -0500

    scrape s3

---
+++ scrapes3.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	awsRegion  = "us-east-1"
+	bucketName = "images.mattbutterfield.com"
+	dbFileName = "app.db"
+	maxKeys    = 100
+)
+
+var (
+	db *sql.DB
+)
+
+func main() {
+	fmt.Println("Hello.")
+	var err error
+	db, err = sql.Open("sqlite3", dbFileName)
+	if err != nil {
+		panic(err)
+	}
+	latestID, err := getLatestID()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Latest id: ", latestID)
+	err = fetchImages(latestID)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getLatestID() (id string, err error) {
+	err = db.QueryRow("SELECT id FROM images ORDER BY created_at DESC LIMIT 1").Scan(&id)
+	return
+}
+
+func fetchImages(latestID string) error {
+	svc := s3.New(session.New(&aws.Config{Region: aws.String(awsRegion)}))
+	result, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: aws.String(bucketName),
+		Marker: aws.String(latestID),
+		MaxKeys: aws.Int64(maxKeys),
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Println("Keys:")
+	for _, result := range result.Contents {
+		fmt.Printf("%s : %s\n", aws.StringValue(result.Key), result.LastModified)
+	}
+	return nil
+}
