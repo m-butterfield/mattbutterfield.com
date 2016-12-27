e36a2db2b46eee7d40ebbb75858d13a07dbb4977
Matt Butterfield <matt.butterfield@percolate.com>
2016-12-27 00:20:34 -0500

    dont redirect to index for new images

--- datastore/image.go
+++ datastore/image.go
@@ -2,7 +2,6 @@ package datastore
 
 import (
 	"database/sql"
-	"encoding/base64"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,26 +11,19 @@ const (
 	getImageByIDQuery    = baseSelectImageQuery + "WHERE id = ? "
 	getLatestImageQuery  = baseSelectImageQuery + "ORDER BY id DESC LIMIT 1"
 	getRandomImageQuery  = baseSelectImageQuery + "WHERE id = (SELECT id FROM images ORDER BY RANDOM() LIMIT 1) "
-	imageBaseURL         = "http://images.mattbutterfield.com/"
 	insertImageQuery     = "INSERT INTO images (id, caption) VALUES (?, ?)"
 )
 
 type Image struct {
 	ID      string
-	URL     string
 	Caption string
 }
 
 func NewImage(id, caption string) *Image {
 	return &Image{
 		ID:      id,
-		URL:     imageBaseURL + id,
 		Caption: caption,
 	}
-}
-
-func (I Image) EncodeID() string {
-	return base64.StdEncoding.EncodeToString([]byte(I.ID))
 }
 
 func (I Image) Save() error {
@@ -89,12 +81,4 @@ func makeImageFromRow(row *sql.Row) (*Image, error) {
 		return nil, err
 	}
 	return NewImage(id, caption.String), nil
-}
-
-func DecodeImageID(encodedID string) (string, error) {
-	imageID, err := base64.StdEncoding.DecodeString(encodedID)
-	if err != nil {
-		return "", err
-	}
-	return string(imageID), nil
 }
--- website/website.go
+++ website/website.go
@@ -1,6 +1,7 @@
 package website
 
 import (
+	"encoding/base64"
 	"fmt"
 	"html/template"
 	"net"
@@ -12,14 +13,30 @@ import (
 )
 
 const (
+	imageBaseURL      = "http://images.mattbutterfield.com/"
+	imagePathBase     = "/img/"
 	imageTemplateName = "website/templates/image.html"
 	port              = "8000"
 )
 
+type ImagePage struct {
+	ImageCaption  string
+	ImageURL      string
+	NextImagePath string
+}
+
+func NewImagePage(image, nextImage *datastore.Image) *ImagePage {
+	return &ImagePage{
+		ImageCaption:  image.Caption,
+		ImageURL:      imageBaseURL + image.ID,
+		NextImagePath: imagePathBase + encodeImageID(nextImage.ID),
+	}
+}
+
 func Run() error {
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
-	r.HandleFunc("/img/{id}", img)
+	r.HandleFunc(imagePathBase+"{id}", img)
 	fmt.Println("Serving on port: ", port)
 	err := http.ListenAndServe(net.JoinHostPort("", port), r)
 	if err != nil {
@@ -33,11 +50,11 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "error fetching image", http.StatusInternalServerError)
 	}
-	http.Redirect(w, r, "/img/"+image.EncodeID(), http.StatusFound)
+	http.Redirect(w, r, makeImagePath(image.ID), http.StatusFound)
 }
 
 func img(w http.ResponseWriter, r *http.Request) {
-	id, err := datastore.DecodeImageID(mux.Vars(r)["id"])
+	id, err := decodeImageID(mux.Vars(r)["id"])
 	if err != nil {
 		http.Error(w, "invalid image id", http.StatusInternalServerError)
 		return
@@ -47,11 +64,32 @@ func img(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "error fetching image", http.StatusInternalServerError)
 		return
 	}
+	nextImage, err := datastore.GetRandomImage()
+	if err != nil {
+		http.Error(w, "error fetching next image", http.StatusInternalServerError)
+	}
 	tmpl, err := template.ParseFiles(imageTemplateName)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "error fetching template", http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w, image)
+	imagePage := NewImagePage(image, nextImage)
+	tmpl.Execute(w, imagePage)
+}
+
+func makeImagePath(imageID string) string {
+	return imagePathBase + encodeImageID(imageID)
+}
+
+func decodeImageID(encodedID string) (string, error) {
+	imageID, err := base64.StdEncoding.DecodeString(encodedID)
+	if err != nil {
+		return "", err
+	}
+	return string(imageID), nil
+}
+
+func encodeImageID(imageID string) string {
+	return base64.StdEncoding.EncodeToString([]byte(imageID))
 }
