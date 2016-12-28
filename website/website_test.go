f7a352dab7af366433c24cb4600f97dd3a0bbd53
Matt Butterfield <matt.butterfield@percolate.com>
2016-12-27 22:31:30 -0500

    check header location

--- website/website_test.go
+++ website/website_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/m-butterfield/mattbutterfield.com/datastore"
@@ -22,7 +23,7 @@ func TestIndex(t *testing.T) {
 	db_mock.ExpectQuery(datastore.SelectRandomImageRegex).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "caption"}).AddRow(imageID, ""))
 
-	r, err := http.NewRequest(http.MethodGet, imagePathBase+encodeImageID(imageID), nil)
+	r, err := http.NewRequest(http.MethodGet, "", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -30,6 +31,13 @@ func TestIndex(t *testing.T) {
 	index(w, r)
 	if w.Code != http.StatusFound {
 		t.Errorf("Unexpected return code: %s", w.Code)
+	}
+	if value, ok := w.Header()["Location"]; ok {
+		if !strings.HasSuffix(value[0], imagePathBase+encodeImageID(imageID)) {
+			t.Errorf("Unexpected redirect location: %s", value)
+		}
+	} else {
+		t.Error("Location header not found in response.")
 	}
 	if err := db_mock.ExpectationsWereMet(); err != nil {
 		t.Error(err)
@@ -50,10 +58,11 @@ func TestImg(t *testing.T) {
 	imageTemplateName = cwd + "/" + "templates/image.html"
 
 	imageID := "1234"
-	db_mock.ExpectQuery(datastore.SelectImageByIDRegex).
+	randImageID := "blerp"
+	db_mock.ExpectQuery(datastore.SelectImageByIDRegex).WithArgs(imageID).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "caption"}).AddRow(imageID, ""))
 	db_mock.ExpectQuery(datastore.SelectRandomImageRegex).
-		WillReturnRows(sqlmock.NewRows([]string{"id", "caption"}).AddRow(imageID, ""))
+		WillReturnRows(sqlmock.NewRows([]string{"id", "caption"}).AddRow(randImageID, ""))
 
 	r, err := http.NewRequest(http.MethodGet, imagePathBase+encodeImageID(imageID), nil)
 	if err != nil {
