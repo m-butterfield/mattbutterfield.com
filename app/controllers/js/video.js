882fc4b7aa3fe7f59a0d8acf0623d20e6e48c87a
Matt Butterfield <matt@mattbutterfield.com>
2021-05-03 18:56:37 -0400

    move ws init

--- app/controllers/js/video.js
+++ app/controllers/js/video.js
@@ -1,6 +1,30 @@
 let peerConnection = new RTCPeerConnection({"iceServers": [{"urls": "stun:stun.l.google.com:19302"}]}),
     ws = new WebSocket((window.location.protocol === "https:" ? "wss://" : "ws://") + window.location.host + '/video/connections' + window.location.search);
 
+ws.onmessage = (evt) => {
+  const message = JSON.parse(evt.data);
+  switch (message.type) {
+    case 'offer': {
+      peerConnection.setRemoteDescription(message).then(() => {
+        return peerConnection.createAnswer()
+      }).then(answer => {
+        return peerConnection.setLocalDescription(answer)
+      }).then(() => {
+        ws.send(JSON.stringify(peerConnection.localDescription));
+      });
+      break;
+    }
+    case 'answer': {
+      peerConnection.setRemoteDescription(message);
+      break;
+    }
+    case 'candidate': {
+      peerConnection.addIceCandidate(new RTCIceCandidate(message.ice));
+      break;
+    }
+  }
+};
+
 navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
   let element = document.getElementById('local_video');
   element.srcObject = stream;
@@ -25,28 +49,4 @@ peerConnection.ontrack = evt => {
 
 peerConnection.onicecandidate = evt => {
   if (evt.candidate) ws.send(JSON.stringify({type: 'candidate', ice: evt.candidate}));
-};
-
-ws.onmessage = (evt) => {
-  const message = JSON.parse(evt.data);
-  switch (message.type) {
-    case 'offer': {
-      peerConnection.setRemoteDescription(message).then(() => {
-        return peerConnection.createAnswer()
-      }).then(answer => {
-        return peerConnection.setLocalDescription(answer)
-      }).then(() => {
-        ws.send(JSON.stringify(peerConnection.localDescription));
-      });
-      break;
-    }
-    case 'answer': {
-      peerConnection.setRemoteDescription(message);
-      break;
-    }
-    case 'candidate': {
-      peerConnection.addIceCandidate(new RTCIceCandidate(message.ice));
-      break;
-    }
-  }
 };
