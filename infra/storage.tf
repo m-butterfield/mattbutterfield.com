2fb5cf1d159dc618ce5240577d2d7ac639564250
Matt Butterfield <matt@mattbutterfield.com>
2022-07-23 16:10:25 -0400

    add certs and backend buckets

--- infra/storage.tf
+++ infra/storage.tf
@@ -24,14 +24,14 @@ resource "google_storage_bucket" "files" {
 }
 
 resource "google_storage_bucket" "images" {
-  name     = "images.mattbutterfield.com"
-  location = "US"
+  name                        = "images.mattbutterfield.com"
+  location                    = "US"
   uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket_iam_member" "images_public" {
   bucket = google_storage_bucket.images.name
-  role = "roles/storage.objectViewer"
+  role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
 
@@ -47,12 +47,40 @@ resource "google_project_iam_member" "storage_object_admin" {
   member  = "serviceAccount:${google_service_account.mattbutterfield_cloud_run.email}"
 }
 
-resource "google_compute_global_address" "images" {
+# start of https url forwarding to bucket content
+
+resource "google_compute_managed_ssl_certificate" "images" {
   name = "images"
+
+  managed {
+    domains = ["images.mattbutterfield.com"]
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "files" {
+  name = "files"
+
+  managed {
+    domains = ["files.mattbutterfield.com"]
+  }
+}
+
+resource "google_compute_global_address" "images" {
+  name    = "images"
   address = "34.98.91.63"
 }
 
 resource "google_compute_global_address" "files" {
-  name = "files"
+  name    = "files"
   address = "34.120.4.174"
+}
+
+resource "google_compute_backend_bucket" "images" {
+  name        = "images"
+  bucket_name = google_storage_bucket.images.name
+}
+
+resource "google_compute_backend_bucket" "files" {
+  name        = "files"
+  bucket_name = google_storage_bucket.files.name
 }
